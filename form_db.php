2415637459613521bd03edd1e6e1ff49c212994a
dhananjay-tribugs <dhananjay.nalawade@tribugs.com>
2022-06-02 18:49:13 +0530

    my first commit

---
+++ form_db.php
@@ -0,0 +1,23 @@
+
+
+<html>
+<body>
+
+<form name="register" action="insert.php" method="post">
+
+<fieldset><legend>Registration</legend>
+
+	<p>Name: <input type="text" name="name" id="Name"/></p>
+	
+	<p>Email: <input type="text" name="email" id="Email"/></p>
+	
+	<p>Phone: <input type="text" name="phone" id="Phone"/></p>
+	
+	<p>City: <input type="text" name="city" id="City"/></p>
+		
+	<p><input type="submit" name="submit" value="Sign In"/></p>
+
+</form>
+
+</body>
+</html>
